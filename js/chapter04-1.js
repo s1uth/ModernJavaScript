ff0037fd38bd4f685f51530e3f6eb1cbcd90eece
s1uth <handtaein@gmail.com>
2022-12-28 12:21:42 +0900

    챕터 004 마무리 문제 1

---
+++ js/chapter04-1.js
@@ -0,0 +1,20 @@
+document.write("<h1>양수인가? 음수인가?</h1>")
+
+const inputNum = prompt("숫자를 입력해주세요")
+
+const identify = (props) =>{
+    if(props<0){
+        alert(props + "는 음수입니다.")
+    }else if(props>0){
+        alert(props + "는 양수입니다.")
+    }else{
+        alert("0 입니다.")
+    }
+}
+
+if(isNaN(inputNum)){
+    inputNum = prompt("숫자를 입력해주세요")
+}
+else{
+    identify(inputNum)
+}
