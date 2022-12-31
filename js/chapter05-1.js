8e51d2e271e490d88fb9026d46a1e10a912121e0
s1uth <handtaein@gmail.com>
2023-01-01 03:43:38 +0900

    챕터 005 마무리 문제 1

---
+++ js/chapter05-1.js
@@ -0,0 +1,7 @@
+const BtnView = document.querySelector('button #view')
+const contents = document.querySelector('div #detail')
+
+
+ BtnView.onClick = () => {
+    contents.clas.toggle('hidden')  
+}
